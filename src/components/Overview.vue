1abf7af91ef6693c955d3fb6478c27c275e76428
Webia1 <erkan.buelbuel@gmail.com>
2018-11-19 23:50:55 +0100

    example1 with buttons

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,10 +1,26 @@
 <template>
-  <div>
-    <h1>
-      Lieber Kunde, was können wir heute
-      für dich tun?
-    </h1>
-  </div>
+  <v-layout>
+    <v-flex>
+      <div>
+        <h1>
+          Lieber Kunde, was können wir heute
+          für dich tun?
+        </h1>
+      </div>
+      <div>
+        <v-btn color="red" dark>Meine Versicherungen</v-btn>
+      </div>
+      <div>
+        <v-btn color="red" dark>Meine Daten</v-btn>
+      </div>
+      <div>
+        <v-btn color="red" dark>Mein Fall</v-btn>
+      </div>
+      <div>
+        <v-btn color="red" dark>Ergo in meiner Nähe</v-btn>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script lang="ts">
@@ -20,7 +36,7 @@ export default class Overview extends Vue {
 
 <style scoped lang="stylus">
 h1 {
-  margin: 40px 0 0;
+  margin: 40px 0;
   color: red;
 }
 </style>
