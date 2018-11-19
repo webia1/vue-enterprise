3dd3270ffc669d3fc5655c19304de3d1177e0ec4
Webia1 <erkan.buelbuel@gmail.com>
2018-11-19 20:42:49 +0100

    vuetify plugin tslint rules update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import '@babel/polyfill'
+import '@babel/polyfill';
 import Vue from 'vue';
-import './plugins/vuetify'
+import './plugins/vuetify';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -10,5 +10,5 @@ Vue.config.productionTip = false;
 new Vue({
   router,
   store,
-  render: (h) => h(App),
+  render: h => h(App),
 }).$mount('#app');
