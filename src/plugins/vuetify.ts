841450797ea87a8626f2c9c1b2db140caa6cf547
Webia1 <erkan.buelbuel@gmail.com>
2018-11-19 20:37:14 +0100

    Vuetify added

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import de from 'vuetify/src/locale/de'
+
+Vue.use(Vuetify, {
+  customProperties: true,
+  iconfont: 'md',
+  lang: {
+    locales: { de },
+    current: 'de'
+  },
+})
