5fb797e9a6d18fd82fe6c776b4238944a7322e71
NonPolynomial <NonPolynomial@users.noreply.github.com>
2018-12-17 12:52:05 +0100

    added input modes to phone number field

--- src/components/forms/phone-number-field/index.vue
+++ src/components/forms/phone-number-field/index.vue
@@ -1,26 +1,31 @@
 <template>
   <v-layout>
-    <v-flex xs3>
-      <!-- <v-select
-        box
-        label="Landesvorwahl"
-        :items="countryCodes"
-        :rules="[isValid('countryCode')]"
-        v-model="countryCode"
-        @input="emitValue()"
-      /> -->
-      <v-text-field
-        box
-        label="Landesvorwahl"
-        v-model="countryCode"
-        ref="countryCodeField"
-        @input="emitValue()"
-      >
+    <template v-if="mode === 'modern'">
+      <v-flex xs2>
+        <!-- <v-select
+          box
+          label="Landesvorwahl"
+          :items="countryCodes"
+          :rules="[isValid('countryCode')]"
+          v-model="countryCode"
+          @input="emitValue()"
+        /> -->
         <v-menu
           full-width
-          slot="append"
+          :open-on-click="false"
+          v-model="showCountryCodeSelection"
         >
-          <v-icon slot="activator">arrow_drop_down</v-icon>
+          <v-text-field
+            box
+            append-icon="arrow_drop_down"
+            label="Landesvorwahl"
+            v-model="countryCode"
+            slot="activator"
+            @paste="formatCountryCode()"
+            @input="emitValue()"
+            @blur="formatCountryCode()"
+            @click:append="openCountryCodeSelection()"
+          />
           <v-list>
             <v-list-tile
               v-for="code in countryCodes"
@@ -33,26 +38,53 @@
             </v-list-tile>
           </v-list>
         </v-menu>
-      </v-text-field>
-    </v-flex>
-    <v-flex xs3>
-      <v-text-field
-        box
-        label="Vorwahl"
-        hint="ohne führende Null (0)"
-        :rules="[isValid('prefix')]"
-        v-model="prefix"
-        @input="emitValue()"
-      />
-    </v-flex>
-    <v-flex xs6>
-      <v-text-field
-        box
-        label="Rufnummer"
-        :rules="[isValid('phoneNumber')]"
-        v-model="phoneNumber"
-        @input="emitValue()"
-      />
+      </v-flex>
+      <v-flex xs3>
+        <v-text-field
+          box
+          label="Vorwahl"
+          hint="ohne führende Null (0)"
+          :rules="[isValid('prefix')]"
+          v-model="prefix"
+          @input="emitValue()"
+        />
+      </v-flex>
+      <v-flex xs6>
+        <v-text-field
+          box
+          label="Rufnummer"
+          :rules="[isValid('phoneNumber')]"
+          v-model="phoneNumber"
+          @input="emitValue()"
+        />
+      </v-flex>
+    </template>
+    <template v-else-if="mode === 'classic'">
+      <v-flex xs11>
+        <v-text-field
+          box
+          label="Telefonnummer"
+          v-model="fullValue"
+        />
+      </v-flex>
+    </template>
+    <v-flex xs1>
+      <v-tooltip
+        bottom
+      >
+        <v-btn
+          icon
+          large
+          slot="activator"
+        >
+          <v-icon
+            @click="changeMode()"
+          >
+            edit
+          </v-icon>
+        </v-btn>
+        Change edit mode
+      </v-tooltip>
     </v-flex>
   </v-layout>
 </template>
@@ -63,13 +95,20 @@
 
   import countryCodes from './countryCodes.json';
 
-  @Component({})
+  @Component({
+    filters: {
+      formatCountryCode: a => a.replace(/^00/g, '+'),
+    },
+  })
   export default class PhoneNumberField extends Vue {
+    private mode: 'modern' | 'classic' = 'modern';
     private countryCodes = countryCodes
     private countryCode = '';
     private prefix = '';
     private phoneNumber = '';
+    private fullValue = '';
     private errors = {};
+    private showCountryCodeSelection = false;
 
     private get countryCodeField() {
       if (!this.$refs.countryCodeField) return null;
@@ -98,6 +137,10 @@
       },
     };
 
+    private changeMode() {
+      this.mode = (this.mode === 'modern' && 'classic') || 'modern';
+    }
+
     private isValid(fieldName: string) {
       return () => {
         if (this[fieldName] !== '' && this.errors[fieldName]) {
@@ -109,18 +152,27 @@
     }
 
     private emitValue() {
+      this.formatCountryCode();
       const errors = validate({
         countryCode: this.countryCode,
         prefix: this.prefix,
         phoneNumber: this.phoneNumber,
       }, this.validationConstraints, { fullMessages: false });
-      console.dir(errors);
       if (!errors) {
         this.errors = {};
         this.$emit('input', `${this.countryCode}${this.prefix}${(this.countryCode || this.prefix) && this.phoneNumber ? '/': ''}${this.phoneNumber}`);
       } else {
         this.errors = errors;
+        this.$emit('input', null);
       }
+    }
+
+    private formatCountryCode() {
+      this.countryCode = (this.$options.filters as any).formatCountryCode(this.countryCode);
+    }
+
+    private openCountryCodeSelection() {
+      this.showCountryCodeSelection = true;
     }
   }
 </script>
